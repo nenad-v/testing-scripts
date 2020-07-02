7e252fef5399b9566f7ea3f4b8606d08c69a96da
Nenad <nvracar@ebay.com>
2020-07-02 11:05:48 +0200

    added test-script-01

---
+++ test-script-01.js
@@ -0,0 +1,6 @@
+const element = document.querySelector('.tef-slider');
+const heading = document.createElement('h1');
+
+heading.textContent = 'RANDOM ELEMENT';
+heading.style.fontSize = '100px';
+element.appendChild(heading);
